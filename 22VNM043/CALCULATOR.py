49666629f1eb3d4e2f880e8b7262af6b3abdb6c0
siva M <164191405+siva23042005@users.noreply.github.com>
2024-06-05 12:09:02 +0530

    Add files via upload

---
+++ 22VNM043/CALCULATOR.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter the num1:"))
+num2 = eval(input("Enter the num2:"))
+act = input()
+if 1:
+    add = num1 + num2
+    print(add)
+elif 2:
+    sub = num1 - num2
+    print(sub)
+elif 3:
+    mul = num1 * num2
+    print(mul)
+elif 4:
+    div = num1 / num2
+    print(div)
+    
